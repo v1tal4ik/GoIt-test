32fbd01191a64877bbdb9ed6801f67c2a4e325b4
v1tal4ik <vetalgta@gmail.com>
2023-02-22 11:05:32 +0200

    remove axios inteceptor, add gh-pages package

--- src/api/config.js
+++ src/api/config.js
@@ -5,20 +5,4 @@ const HTTP = axios.create({
 	baseURL: API.BASE,
 });
 
-HTTP.interceptors.request.use(
-	async (config) => {
-		const { headers } = config;
-
-		return {
-			...config,
-			headers: {
-				...headers,
-				Accept: 'application/vnd.github+json',
-				Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
-			},
-		};
-	},
-	(error) => Promise.reject(error)
-);
-
 export default HTTP;
